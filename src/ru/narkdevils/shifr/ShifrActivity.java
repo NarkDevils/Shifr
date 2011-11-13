2e1f29126513d476a0de1b7935f8bfa3dad3f5ad
‡Nark†Devils‡ <nark_devils@mail.ru>
2011-11-14 00:09:53 +0900

    add layouts & onclicklistener

--- src/ru/narkdevils/shifr/ShifrActivity.java
+++ src/ru/narkdevils/shifr/ShifrActivity.java
@@ -2,12 +2,46 @@ package ru.narkdevils.shifr;
 
 import android.app.Activity;
 import android.os.Bundle;
+import android.view.View;
+import android.view.View.OnClickListener;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.TextView;
 
-public class ShifrActivity extends Activity {
-    /** Called when the activity is first created. */
-    @Override
+public class ShifrActivity extends Activity implements OnClickListener {
+	
+	EditText editText1;
+	TextView textView1;
+	Button button1;
+	
     public void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.main);
+        
+        editText1 = (EditText) findViewById(R.id.editText1);
+        textView1 = (TextView) findViewById(R.id.textView1);
+        button1 = (Button) findViewById(R.id.button1);
+        button1.setOnClickListener(this);
+        
     }
+
+	public void onClick(View v) {
+		switch (v.getId()) {
+			//case R.id.radio0:editText2.setText("Нажата кнопка ОК");
+			//break;
+			//case R.id.radio1:editText2.setText("Нажата кнопка Cancel");
+			//break;
+			case R.id.button1:
+	  			char[] csText = Crypt(editText1.getText().toString().toCharArray());
+	  			CharSequence seq = new String(csText);
+	  			textView1.setText(seq);
+			break;
+		}
+		
+	}
+
+	private char[] Crypt(char[] charArray) {
+		// TODO Auto-generated method stub
+		return null;
+	}
 }
