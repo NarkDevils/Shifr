9e451ea173dbc998d59f91a060242efd1ddf577c
‡Nark†Devils‡ <nark_devils@mail.ru>
2011-11-13 23:44:37 +0900

    add commit

---
+++ src/ru/narkdevils/shifr/ShifrActivity.java
@@ -0,0 +1,13 @@
+package ru.narkdevils.shifr;
+
+import android.app.Activity;
+import android.os.Bundle;
+
+public class ShifrActivity extends Activity {
+    /** Called when the activity is first created. */
+    @Override
+    public void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.main);
+    }
+}
