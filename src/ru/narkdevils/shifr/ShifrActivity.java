2bbf40622fd9b27b020680c02c8e59f152943eeb
‡Nark†Devils‡ <nark_devils@mail.ru>
2011-11-15 03:06:58 +0900

    add support uppercase

--- src/ru/narkdevils/shifr/ShifrActivity.java
+++ src/ru/narkdevils/shifr/ShifrActivity.java
@@ -28,7 +28,7 @@ public class ShifrActivity extends Activity implements OnClickListener {
         
     }
     
-    private char[][] table = {
+    private char[][] table_lc = {
     		{'a','b','c','d','e','f','g','h','i','j','k','l','m','n','o','p','q','r','s','t','u','v','w','x','y','z'},
     		{'b','c','d','e','f','g','h','i','j','k','l','m','n','o','p','q','r','s','t','u','v','w','x','y','z','a'},
     		{'c','d','e','f','g','h','i','j','k','l','m','n','o','p','q','r','s','t','u','v','w','x','y','z','a','b'},
@@ -57,7 +57,37 @@ public class ShifrActivity extends Activity implements OnClickListener {
     		{'z','a','b','c','d','e','f','g','h','i','j','k','l','m','n','o','p','q','r','s','t','u','v','w','x','y'}
     		};
     
-    private char[] key = {'n', 'a', 'r', 'k', 'd', 'e', 'v', 'i', 'l', 's'};
+	private char[][] table_uc = {
+			{'A','B','C','D','E','F','G','H','I','J','K','L','M','N','O','P','Q','R','S','T','U','V','W','X','Y','Z'},
+    		{'B','C','D','E','F','G','H','I','J','K','L','M','N','O','P','Q','R','S','T','U','V','W','X','Y','Z','A'},
+    		{'C','D','E','F','G','H','I','J','K','L','M','N','O','P','Q','R','S','T','U','V','W','X','Y','Z','A','B'},
+    		{'D','E','F','G','H','I','J','K','L','M','N','O','P','Q','R','S','T','U','V','W','X','Y','Z','A','B','C'},
+    		{'E','F','G','H','I','J','K','L','M','N','O','P','Q','R','S','T','U','V','W','X','Y','Z','A','B','C','D'},
+    		{'F','G','H','I','J','K','L','M','N','O','P','Q','R','S','T','U','V','W','X','Y','Z','A','B','C','D','E'},
+    		{'G','H','I','J','K','L','M','N','O','P','Q','R','S','T','U','V','W','X','Y','Z','A','B','C','D','E','F'},
+    		{'H','I','J','K','L','M','N','O','P','Q','R','S','T','U','V','W','X','Y','Z','A','B','C','D','E','F','G'},
+    		{'I','J','K','L','M','N','O','P','Q','R','S','T','U','V','W','X','Y','Z','A','B','C','D','E','F','G','H'},
+    		{'J','K','L','M','N','O','P','Q','R','S','T','U','V','W','X','Y','Z','A','B','C','D','E','F','G','H','I'},
+    		{'K','L','M','N','O','P','Q','R','S','T','U','V','W','X','Y','Z','A','B','C','D','E','F','G','H','I','J'},
+    		{'L','M','N','O','P','Q','R','S','T','U','V','W','X','Y','Z','A','B','C','D','E','F','G','H','I','J','K'},
+    		{'M','N','O','P','Q','R','S','T','U','V','W','X','Y','Z','A','B','C','D','E','F','G','H','I','J','K','L'},
+    		{'N','O','P','Q','R','S','T','U','V','W','X','Y','Z','A','B','C','D','E','F','G','H','I','J','K','L','M'},
+    		{'O','P','Q','R','S','T','U','V','W','X','Y','Z','A','B','C','D','E','F','G','H','I','J','K','L','M','N'},
+    		{'P','Q','R','S','T','U','V','W','X','Y','Z','A','B','C','D','E','F','G','H','I','J','K','L','M','N','O'},
+    		{'Q','R','S','T','U','V','W','X','Y','Z','A','B','C','D','E','F','G','H','I','J','K','L','M','N','O','P'},
+    		{'R','S','T','U','V','W','X','Y','Z','A','B','C','D','E','F','G','H','I','J','K','L','M','N','O','P','Q'},
+    		{'S','T','U','V','W','X','Y','Z','A','B','C','D','E','F','G','H','I','J','K','L','M','N','O','P','Q','R'},
+    		{'T','U','V','W','X','Y','Z','A','B','C','D','E','F','G','H','I','J','K','L','M','N','O','P','Q','R','S'},
+    		{'U','V','W','X','Y','Z','A','B','C','D','E','F','G','H','I','J','K','L','M','N','O','P','Q','R','S','T'},
+    		{'V','W','X','Y','Z','A','B','C','D','E','F','G','H','I','J','K','L','M','N','O','P','Q','R','S','T','U'},
+    		{'W','X','Y','Z','A','B','C','D','E','F','G','H','I','J','K','L','M','N','O','P','Q','R','S','T','U','V'},
+    		{'X','Y','Z','A','B','C','D','E','F','G','H','I','J','K','L','M','N','O','P','Q','R','S','T','U','V','W'},
+    		{'Y','Z','A','B','C','D','E','F','G','H','I','J','K','L','M','N','O','P','Q','R','S','T','U','V','W','X'},
+    		{'Z','A','B','C','D','E','F','G','H','I','J','K','L','M','N','O','P','Q','R','S','T','U','V','W','X','Y'}
+			};
+    
+    private char[] key_lc = {'n', 'a', 'r', 'k', 'd', 'e', 'v', 'i', 'l', 's'};
+    private char[] key_uc = {'N', 'A', 'R', 'K', 'D', 'E', 'V', 'I', 'L', 'S'};
 
 	public void onClick(View v) {
 		switch (v.getId()) {
@@ -81,13 +111,26 @@ public class ShifrActivity extends Activity implements OnClickListener {
 
     	for(int k = 0, ki = 0; k < text.length; k++, ki++)
     	{
-    		 if(ki == key.length) ki=0;
-             for(ij = 0, ch = 'a'; ch <= 'z'; ch++, ij++)
-             {
-            	 if(text[k] == ch) i = ij;
-                 if(key[ki] == ch) j = ij;
-             }
-             if(text[k] != ' ') text[k] = table[i][j];
+    		 if(ki == key_lc.length) ki=0;
+    		 if(text[k] >= 'a' && text[k] <= 'z')
+    		 {
+    			 for(ij = 0, ch = 'a'; ch <= 'z'; ch++, ij++)
+    			 {
+    				 if(text[k] == ch) i = ij;
+    				 if(key_lc[ki] == ch) j = ij;
+    			 }
+    		 	 if(text[k] != ' ') text[k] = table_lc[i][j];
+    		 }
+    		 else if(text[k] >= 'A' && text[k] <= 'Z')
+    		 {
+    			 for(ij = 0, ch = 'A'; ch <= 'Z'; ch++, ij++)
+                 {
+                	 if(text[k] == ch) i = ij;
+                     if(key_uc[ki] == ch) j = ij;
+                 }
+    		 	 if(text[k] != ' ') text[k] = table_uc[i][j];
+    		 }
+             
     	}
 		return text;
 	}
@@ -98,11 +141,11 @@ public class ShifrActivity extends Activity implements OnClickListener {
 
 		for(int k = 0, ki = 0; k != text.length; k++, ki++)
 		{
-	         if(ki == key.length) ki = 0; 
+	         if(ki == key_lc.length) ki = 0; 
 			 for(ij = 0, ch = 'a'; ch <= 'z'; ch++, ij++)
-	              if(key[ki] == ch) j = ij;
+	              if(key_lc[ki] == ch) j = ij;
 	         for(ij = 0, ch = 'a'; ch <= 'z'; ch++, ij++)
-	              if(text[k] == table[ij][j]) { text[k] = ch; break; }
+	              if(text[k] == table_lc[ij][j]) { text[k] = ch; break; }
 		}
 		
 		return text;
