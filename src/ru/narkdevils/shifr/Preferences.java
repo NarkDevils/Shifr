c8fe3af9b769cf1a2c495b60061d454d89ea391c
‡Nark†Devils‡ <nark_devils@mail.ru>
2011-12-03 23:55:38 +0900

    admob add

--- src/ru/narkdevils/shifr/Preferences.java
+++ src/ru/narkdevils/shifr/Preferences.java
@@ -4,7 +4,7 @@ import android.os.Bundle;
 import android.preference.PreferenceActivity;
 
 public class Preferences extends PreferenceActivity {
-	
+
 	@Override
 	public void onCreate(Bundle savedInstanceState) {
 		super.onCreate(savedInstanceState);
