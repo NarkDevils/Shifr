da24c7554b9978190cfd7c4777a3631e3e9c06a9
‡Nark†Devils‡ <nark_devils@mail.ru>
2012-06-18 14:12:50 +1000

    fix

--- src/ru/narkdevils/shifr/ShifrActivity.java
+++ src/ru/narkdevils/shifr/ShifrActivity.java
@@ -1,7 +1,6 @@
 package ru.narkdevils.shifr;
 
 import android.app.Activity;
-import android.content.Context;
 import android.content.Intent;
 import android.content.SharedPreferences;
 import android.os.Bundle;
