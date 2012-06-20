eb3c595032dd00658896de352344f4181c5a1fd7
‡Nark†Devils‡ <nark_devils@mail.ru>
2012-06-21 00:46:53 +1000

    fix lib

--- src/ru/narkdevils/shifr/ShifrActivity.java
+++ src/ru/narkdevils/shifr/ShifrActivity.java
@@ -17,7 +17,6 @@ import android.view.View;
 import android.view.View.OnClickListener;
 import android.widget.Button;
 import android.widget.EditText;
-import android.widget.Toast;
 
 public class ShifrActivity extends Activity implements OnClickListener {
 
